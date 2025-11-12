732e9a13e9fd8c10eaa4cd26c4bfd8c4249db7be
Techno Horizon <th-deploy@panel.thdevops.co.in>
2025-11-12 11:55:26 +0000

    (1) Urls added for frontend (2) Seriliazers added to display users role name as well (3) Added CORS Allow Credentails in Settings

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,8 +1,15 @@
-# core/api/serializers.py
 from rest_framework import serializers
 from core.models import MasterUser
 
 class MasterUserSerializer(serializers.ModelSerializer):
+    role_name = serializers.SerializerMethodField()
+
     class Meta:
         model = MasterUser
-        fields = ['id', 'username', 'role', 'recovery_mobile', 'TH_urid']
+        fields = ['id', 'username', 'role', 'role_name', 'recovery_mobile', 'TH_urid']
+
+    def get_role_name(self, obj):
+        try:
+            return obj.role.name if obj.role else None
+        except Exception:
+            return None
