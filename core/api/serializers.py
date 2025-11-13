869682e09b709bbfecfd6ddf663550ebfbdb5f3c
Techno Horizon <th-deploy@panel.thdevops.co.in>
2025-11-13 19:21:52 +0000

    Fixed Rest Serializers whose source was blocking response

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -144,9 +144,6 @@ class MasterStateSerializer(serializers.ModelSerializer):
 
 
 class MasterMandalSerializer(serializers.ModelSerializer):
-    created_by = serializers.IntegerField(source='created_by_id', read_only=True)
-    updated_by = serializers.IntegerField(source='updated_by_id', read_only=True)
-    deleted_by = serializers.IntegerField(source='deleted_by_id', read_only=True)
 
     class Meta:
         model = models.MasterMandal
@@ -258,9 +255,6 @@ class MasterRolesSerializer(serializers.ModelSerializer):
 
 class MasterUserSerializer(serializers.ModelSerializer):
     role_id = serializers.IntegerField(read_only=True)
-    created_by = serializers.IntegerField(read_only=True)
-    updated_by = serializers.IntegerField(read_only=True)
-    deleted_by = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = models.MasterUser
