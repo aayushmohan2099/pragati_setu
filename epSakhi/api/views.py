84ddabac0d5be991a0f5b73a6373f3b576cc6ff2
Techno Horizon <th-deploy@panel.thdevops.co.in>
2025-11-18 16:51:25 +0000

    Made 1 bugfix to add Multiparser library

--- epSakhi/api/views.py
+++ epSakhi/api/views.py
@@ -17,6 +17,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
+from rest_framework.parsers import MultiPartParser, FormParser
 
 from core.models import (
     MasterUser,
